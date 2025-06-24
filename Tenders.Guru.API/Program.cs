c653182c5c2eeb1df24f0177c104884353bcda39
Rafał Darłak <contact@rafaldarlak.com>
2025-06-24 22:34:56 +0200

    Add initial project structure with configuration files and program entry point

---
+++ Tenders.Guru.API/Program.cs
@@ -0,0 +1,29 @@
+namespace Tenders.Guru.API;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        
+        builder.Services.AddControllers();
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
